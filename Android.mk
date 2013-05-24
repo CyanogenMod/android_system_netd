f6f30d5434f958a61993d2b9436c666edacf89fa
Alin Jerpelea <jerpelea@gmail.com>
2013-05-24 18:26:11 +0300

    netd: permit independent use of ti softap
    Change-Id: I8524161d91a28f3e3facc91a6ebb3c8a1c1ca9c8

--- Android.mk
+++ Android.mk
@@ -41,12 +41,15 @@ LOCAL_CFLAGS := -Werror=format
 LOCAL_SHARED_LIBRARIES := libstlport libsysutils libcutils libnetutils \
                           libcrypto libhardware_legacy libmdnssd
 
-ifdef USES_TI_MAC80211
+ifeq ($(BOARD_TI_SOFTAP),true)
+  LOCAL_SRC_FILES += SoftapControllerTI.cpp
+else
+ifeq ($(USES_TI_MAC80211),true)
   LOCAL_SRC_FILES += SoftapControllerTI.cpp
 else
   LOCAL_SRC_FILES += SoftapController.cpp
 endif
-
+endif
 
 ifneq ($(BOARD_HOSTAPD_DRIVER),)
   LOCAL_CFLAGS += -DHAVE_HOSTAPD
