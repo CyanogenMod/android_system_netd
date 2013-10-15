3561466795e2dac4b438df5ccb1a7715e995619e
Steve Kondik <shade@chemlab.org>
2013-10-15 10:11:06 -0700

    netd: Add new flag for QCOM's hostap extension
     * Not everybody cares about this or needs it, so add a flag that can be
       used to include it for devices that include the full stack.
    
    Change-Id: I66d217dec0553924e09592dd3162e930ecfe66fe

--- Android.mk
+++ Android.mk
@@ -58,7 +58,7 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_CFLAGS := $(LOCAL_CFLAGS) -DHAVE_BLUETOOTH
 endif
 
-ifeq ($(BOARD_HAS_QCOM_WLAN), true)
+ifeq ($(BOARD_HAS_QCOM_WLAN_SDK), true)
   LOCAL_SRC_FILES += QualcommSoftapCmd.cpp
   LOCAL_CFLAGS += -DQCOM_WLAN
   LOCAL_SHARED_LIBRARIES += libqsap_sdk
