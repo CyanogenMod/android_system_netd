de5d5df753dd35d852ac47a6174b06eacd0d5523
Sreeram Ramachandran <sreeram@google.com>
2014-07-26 18:43:25 -0700

    Prohibit address families by default unless a VPN explicitly allows them.
    Bug: 15972465
    Change-Id: I3278d94536fefacc86390c1ba4231680f7be8589

--- server/NetworkController.h
+++ server/NetworkController.h
@@ -71,6 +71,9 @@ public:
     int addUsersToNetwork(unsigned netId, const UidRanges& uidRanges) WARN_UNUSED_RESULT;
     int removeUsersFromNetwork(unsigned netId, const UidRanges& uidRanges) WARN_UNUSED_RESULT;
 
+    // |nexthop| can be NULL (to indicate a directly-connected route), "unreachable" (to indicate a
+    // route that's blocked) or a regular IP address.
+    //
     // Routes are added to tables determined by the interface, so only |interface| is actually used.
     // |netId| is given only to sanity check that the interface has the correct netId.
     int addRoute(unsigned netId, const char* interface, const char* destination,
